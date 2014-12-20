0d545dc61436b4c71ef8fd965bb711c0f57c0908
jurre <jurrestender@hotmail.com>
2014-12-20 13:49:42 +0100

    allow CORS requests from all the origins

--- app/routes/base.rb
+++ app/routes/base.rb
@@ -2,6 +2,11 @@ module TodoAPI::Routes
   class Base < Sinatra::Application
     before do
       content_type "application/hal+json"
+      headers "Access-Control-Allow-Origin" => "*",
+        "Access-Control-Allow-Methods" => %w(OPTIONS GET POST PUT DELETE),
+        "Access-Control-Allow-Headers" => "Content-Type"
     end
+
+    set :protection, false
   end
 end
