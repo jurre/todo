dabcb5ceebd5720b24b4187c64c55eb56c57de73
jurre <jurrestender@hotmail.com>
2014-12-21 20:41:28 +0100

    add spec for unauthorized /todos request

--- spec/requests/todo_request_spec.rb
+++ spec/requests/todo_request_spec.rb
@@ -1,98 +1,107 @@
 require "spec_helper"
 
 describe "Todo endpoint" do
-  let(:user) { Model::User.create(username: "Lana", password: "ants") }
-
-  before(:each) do
-    allow_any_instance_of(TodoAPI::Routes::Todos).to receive(:current_user) { user }
-  end
-
-  describe "GET /todos" do
-    it "returns todos" do
+  context "when not authenticated" do
+    it "returns a 401" do
       get "/todos"
-
-      expect(last_response.status).to eq(200)
+      expect(last_response.status).to eq(401)
     end
   end
 
-  describe "GET /todos/:id" do
-    it "returns a single todo" do
-      todo = Model::Todo.create(title: "Make this endpoint work", user: user)
-      serialized_todo = todo.extend(Representer::Todo).to_json
+  context "when authenticated" do
+    let(:user) { Model::User.create(username: "Lana", password: "ants") }
 
-      get "/todos/#{todo.id}"
-
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq(serialized_todo)
+    before(:each) do
+      allow_any_instance_of(TodoAPI::Routes::Todos).to receive(:current_user) { user }
     end
 
-    it "returns a 404 when not found" do
-      get "/todos/128"
-      expect(last_response.status).to eq(404)
-    end
-  end
+    describe "GET /todos" do
+      it "returns todos" do
+        get "/todos"
 
-  describe "POST /todos" do
-    it "creates a new todo" do
-      todo_params = { title: "Allow people to post new todos" }
-
-      expect {
-        post_json "/todos", todo_params
-      }.to change { Model::Todo.count }.by(1)
-      expect(Model::Todo.last.user).to eq(user)
-      expect(last_response.status).to eq(200)
+        expect(last_response.status).to eq(200)
+      end
     end
 
-    it "returns a 422 for invalid requests" do
-      invalid_params = { some_random_key: "LOL this won't work!" }
+    describe "GET /todos/:id" do
+      it "returns a single todo" do
+        todo = Model::Todo.create(title: "Make this endpoint work", user: user)
+        serialized_todo = todo.extend(Representer::Todo).to_json
 
-      post_json "/todos", invalid_params
-      expect(last_response.status).to eq(400)
-      expect(JSON.parse(last_response.body)).to eq({
-        "title" => ["cannot be empty"]
-      })
-    end
-  end
+        get "/todos/#{todo.id}"
 
-  describe "PATCH /todos/:id" do
-    let(:todo) { 
-      Model::Todo.create(title: "We need to complete todos, right?!", user: user)
-    }
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to eq(serialized_todo)
+      end
 
-    it "updates a todo" do
-      params = { completed: true }
-
-      patch_json "/todos/#{todo.id}", params
-
-      expect(last_response.status).to eq(200)
-      expect(todo.reload.completed).to eq(true)
+      it "returns a 404 when not found" do
+        get "/todos/128"
+        expect(last_response.status).to eq(404)
+      end
     end
 
-    it "returns a 400 for invalid requests" do
-      invalid_params = { title: nil }
+    describe "POST /todos" do
+      it "creates a new todo" do
+        todo_params = { title: "Allow people to post new todos" }
 
-      patch_json "/todos/#{todo.id}", invalid_params
+        expect {
+          post_json "/todos", todo_params
+        }.to change { Model::Todo.count }.by(1)
+        expect(Model::Todo.last.user).to eq(user)
+        expect(last_response.status).to eq(200)
+      end
 
-      expect(last_response.status).to eq(400)
+      it "returns a 422 for invalid requests" do
+        invalid_params = { some_random_key: "LOL this won't work!" }
+
+        post_json "/todos", invalid_params
+        expect(last_response.status).to eq(400)
+        expect(JSON.parse(last_response.body)).to eq({
+          "title" => ["cannot be empty"]
+        })
+      end
     end
 
-    it "returns a 404 when the todo can't be found" do
-      params = { completed: true }
+    describe "PATCH /todos/:id" do
+      let(:todo) { 
+        Model::Todo.create(title: "We need to complete todos, right?!", user: user)
+      }
 
-      patch_json "/todos/123", params
+      it "updates a todo" do
+        params = { completed: true }
 
-      expect(last_response.status).to eq(404)
+        patch_json "/todos/#{todo.id}", params
+
+        expect(last_response.status).to eq(200)
+        expect(todo.reload.completed).to eq(true)
+      end
+
+      it "returns a 400 for invalid requests" do
+        invalid_params = { title: nil }
+
+        patch_json "/todos/#{todo.id}", invalid_params
+
+        expect(last_response.status).to eq(400)
+      end
+
+      it "returns a 404 when the todo can't be found" do
+        params = { completed: true }
+
+        patch_json "/todos/123", params
+
+        expect(last_response.status).to eq(404)
+      end
     end
-  end
 
-  describe "DELETE /todos/:id" do
-    let(:todo) { Model::Todo.create(title: "Allow deleting todos", user: user) }
+    describe "DELETE /todos/:id" do
+      let(:todo) { Model::Todo.create(title: "Allow deleting todos", user: user) }
 
-    it "deletes the todo" do
-      delete "/todos/#{todo.id}"
+      it "deletes the todo" do
+        delete "/todos/#{todo.id}"
 
-      expect(last_response.status).to eq(200)
-      expect(todo.exists?).to eq(false)
+        expect(last_response.status).to eq(200)
+        expect(todo.exists?).to eq(false)
+      end
     end
   end
 end
