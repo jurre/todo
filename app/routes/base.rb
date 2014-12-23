3f7ce1ca485e017f8f571eb6a0a0d3657319aaf2
jurre <jurrestender@hotmail.com>
2014-12-23 10:10:14 +0100

    allow X-Request-With header and return Allow headers

--- app/routes/base.rb
+++ app/routes/base.rb
@@ -5,9 +5,14 @@ module TodoAPI::Routes
     before do
       content_type "application/hal+json"
       headers "Access-Control-Allow-Origin" => "*",
-        "Access-Control-Allow-Methods" => %w(OPTIONS GET POST PUT DELETE),
-        "Access-Control-Allow-Headers" => %w(Content-Type X-Authorization)
+        "Access-Control-Allow-Methods" => %w(OPTIONS GET POST PATCH PUT DELETE),
+        "Access-Control-Allow-Headers" => %w(Content-Type X-Authorization X-Requested-With),
+        "Allow" => %w(OPTIONS GET POST PATCH PUT DELETE)
       merge_json_body_params
+    end
+
+    options "*" do
+      halt 200
     end
 
     set :protection, false
